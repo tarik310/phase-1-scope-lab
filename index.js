61229eefea466de17c5d54dacc377dfb61aff830
Tareq Harh <tareq310harh@gmail.com>
2023-03-22 14:38:19 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(bestcs){
+    bestCustomer = bestcs
+}
+const leastFavoriteCustomer = "haydar"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "zeynap"
+}
